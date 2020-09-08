db79f71d1513177fcf8b816ab2a4a06e2c235d64
Ryan McQuen <rpcm@linux.com>
2020-09-08 15:55:18 -0700

    Add better examples.

--- tonicExample.js
+++ tonicExample.js
@@ -6,7 +6,15 @@
   const testArr = ['Hi', 'World'];
   const splitWord = (word) => word.split('');
 
-  flatMap(testArr, splitWord);
+  console.log(
+    flatMap(testArr, splitWord)
+  );
 
   // => ['H', 'i', 'W', 'o', 'r', 'l', 'd']
+  
+  console.log(
+    flatMap([1, 2, 3, 4], (x) => [x, x * 2])
+  );
+  
+  // => [1, 2, 2, 4, 3, 6, 4, 8]
 })();
